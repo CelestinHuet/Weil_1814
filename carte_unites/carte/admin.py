b631f3cb6d4e61f04e3209a86e14a15f11836d04
CelestinHuet <celestin.huet@gmail.com>
2025-07-05 11:34:35 +0200

    Projet Django

---
+++ carte_unites/carte/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Lieu, Position, Unite
+
+admin.site.register(Lieu)
+admin.site.register(Position)
+admin.site.register(Unite)
---
+++ carte_unites/carte/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Lieu(models.Model):
+    nom = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.nom
+
+
+class Position(models.Model):
+    lieu = models.ForeignKey(Lieu, on_delete=models.CASCADE)
+    date = models.DateField()
+    planifie = models.BooleanField(default=False)
+    justification = models.TextField(blank=True)
+    effectif = models.CharField(max_length=100, blank=True)
+    source = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.lieu.nom} ({self.date})"
+
+
+class Unite(models.Model):
+    nom = models.CharField(max_length=100)
+    positions = models.ManyToManyField(Position, related_name='unites')
+
+    def __str__(self):
+        return self.nom
