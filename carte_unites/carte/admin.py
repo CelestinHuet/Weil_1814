64e8458b28d4c70202eba8a71fe40f665593cd8f
CelestinHuet <celestin.huet@gmail.com>
2025-08-17 10:18:50 +0200

    Admin

--- carte_unites/carte/admin.py
+++ carte_unites/carte/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Lieu, Position, Unite, Subordonne, Commande
+from .models import Lieu, Position, Unite, Subordonne, Commande, Arrow
 
 admin.site.register(Lieu)
 admin.site.register(Position)
-admin.site.register(Unite)
-admin.site.register(Subordonne)
-admin.site.register(Commande)
+admin.site.register(Arrow)
+
+
+
+@admin.register(Unite)
+class UniteAdmin(admin.ModelAdmin):
+    search_fields = ('nom',)
+
+
+@admin.register(Subordonne)
+class SubordonneAdmin(admin.ModelAdmin):
+    search_fields = ('unite_commandant__nom', 'unite_subordonnee__nom',)
+
+
+@admin.register(Commande)
+class CommandeAdmin(admin.ModelAdmin):
+    search_fields = ('general__nom', 'unite_commandee__nom',)
