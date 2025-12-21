d340692964418a5cdf8d0fb2d059562d250bc40b
CelestinHuet <celestin.huet@gmail.com>
2025-12-21 16:22:08 +0100

    en-tête license

--- carte_unites/carte/management/commands/associate_unite_camp.py
+++ carte_unites/carte/management/commands/associate_unite_camp.py
@@ -1,3 +1,14 @@
+"""
+This file is part of 1814parlescartes.
+
+1814parlescartes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+1814parlescartes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 1814parlescartes. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 from django.core.management.base import BaseCommand
 from carte.models import Unite
 from tqdm import tqdm
